28f2810ee7bd0d89d8292b0ceacd60f434de9511
Илья Мысков <iliamyskov@Ilas-MBP.lan>
2024-07-29 10:56:41 +0300

    fix: fix block juri

--- source/js/main.js
+++ source/js/main.js
@@ -5,11 +5,13 @@
 import {initializeFaqAccordion} from './accordion';
 import { initializePriceTabs } from './price-tab';
 import {reviewsSwiperInit} from './swiper-reviews.js';
+import {juriSwiperInit} from './swiper-juri.js';
 
 
 const bootStrap = () => {
   initializePriceTabs();
   initializeFaqAccordion();
+  juriSwiperInit();
   reviewsSwiperInit();
 
 };
