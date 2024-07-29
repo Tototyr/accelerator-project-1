4719ab8f16aa8e9d92936750a2510b112e06a61a
Илья Мысков <iliamyskov@Ilas-MacBook-Pro.local>
2024-07-29 12:40:59 +0300

    fix: bug fixes

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,8 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
 import {initializeFaqAccordion} from './accordion';
 import { initializePriceTabs } from './price-tab';
 import {reviewsSwiperInit} from './swiper-reviews.js';
 import {juriSwiperInit} from './swiper-juri.js';
+
 
 
 const bootStrap = () => {
